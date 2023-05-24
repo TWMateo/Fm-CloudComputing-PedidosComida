423c544b1ce25c32d2e9c2c5eb658db45d01bd9a
TWMateo <fmchancosic@utn.edu.ec>
2023-05-23 19:50:19 -0500

    Agregar archivos de proyecto.

---
+++ Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PedidosComida.Modelos
+{
+    public class Cliente
+    {
+        [Key]
+        public int IdCliente { get; set; }
+        public String cedula { get; set; }
+        public String nombre { get; set; }
+        public String apellido { get; set; }
+        public String telefono { get; set; }
+
+        //Relaciones
+        public List<PedidoComida>? Pedidos { get; set; }
+    }
+}
---
+++ PedidoComida.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosComida.Modelos
+{
+    public class PedidoComida
+    {
+        public int Id { get; set; }
+        public String Descripcion { get; set; }
+
+        public double Total { get; set; }
+
+        //Relaciones
+        public int ClienteIdCliente { get; set; } //FK
+        public Cliente? Cliente { get; set; }
+    }
+}
